03d7ecb37e7950874bfe2e8d5b1eb9816ad26a46
imaaronath <52249064+imaaronath@users.noreply.github.com>
2019-06-28 16:15:51 +0800

    Add files via upload

---
+++ get.js
@@ -0,0 +1,41 @@
+$(function() {
+    $("#username_error_message").hide();
+    $("#password_error_message").hide();
+    $("#roleID_error_message").hide();
+
+    var error_username = false;
+    var error_password = false;
+    var error_roleID = false;
+
+    $("#from_username").focusout(function()
+    {
+        check_username();
+    });
+    $("#from_passwrod").focusout(function()
+    {
+        check_username();
+    });
+    $("#from_roleID").focusout(function()
+    {
+        check_username();
+    });
+
+    function check_username()
+    {
+        var username = new ReqExp(/^[+a-zA-z0-9._-]+@[a-zA-z0-9.-]+\.[a-zA-z]{2,4}$/i);
+
+        if(username.test($("#form_username").val())){
+            $("#username_error_message").hide();
+        }
+        else{
+            $("#username_error_message").html("Invalid email address");
+            $("#username_error_message").show();
+            error_username = true;
+        }
+    }
+
+    function check_password()
+    {
+        var password_length = $("#form_password").val().length;
+    }
+});
